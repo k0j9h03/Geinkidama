ec8d1d2dc5c40f1c85abb74ddeef8905fc991312
k0j9h03 <k0j9h03@naver.com>
2024-02-21 21:42:24 +0900

    multidevice

--- script.js
+++ script.js
@@ -7,21 +7,26 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.width = width;
     canvas.height = height;
 
+    let isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0; // 디바이스 대응
+
+    let inputEvent = isTouchDevice ? 'touchmove' : 'mousemove'; 
+    let startEvent = isTouchDevice ? 'touchstart' : 'mousedown';
+    let endEvent = isTouchDevice ? 'touchend' : 'mouseup';
+
     let mousePos = { x: 0, y: 0 };
     let isMouseDown = false;
     let circleRadius = 0;
 
     const image = new Image();
-    image.src = 'songoku.png'; 
+    image.src = 'songoku.png';
 
-
-    function scalingCircle (circleRadius){
-        if ( circleRadius >= 7 && circleRadius <= 9){
+    function scalingCircle(circleRadius) {
+        if (circleRadius >= 7 && circleRadius <= 9) {
             let risingCircle = 128;
             return risingCircle;
         }
         let risingCircle = Math.pow(2, circleRadius); // 지수함수 = y = 2^x 
-        return risingCircle; 
+        return risingCircle;
     }
 
     function updateCanvas() {
@@ -29,14 +34,13 @@ document.addEventListener('DOMContentLoaded', function() {
         ctx.drawImage(image, mousePos.x - image.width / 2, mousePos.y - image.height / 2);
 
         if (isMouseDown) {
-            circleRadius += 0.1; 
+            circleRadius += 0.1;
             let updatedCircle = scalingCircle(circleRadius);
 
             ctx.beginPath();
-            ctx.arc(mousePos.x, mousePos.y-330, updatedCircle, 0, Math.PI * 2);
+            ctx.arc(mousePos.x, mousePos.y - 330, updatedCircle, 0, Math.PI * 2);
             ctx.fillStyle = 'black';
             ctx.fill();
-
         }
 
         requestAnimationFrame(updateCanvas);
@@ -49,17 +53,22 @@ document.addEventListener('DOMContentLoaded', function() {
         canvas.height = height;
     });
 
-    window.addEventListener('mousemove', function(e) {
-        mousePos.x = e.clientX;
-        mousePos.y = e.clientY;
+    canvas.addEventListener(inputEvent, function(e) {
+        if (isTouchDevice) {
+            mousePos.x = e.touches[0].clientX;
+            mousePos.y = e.touches[0].clientY;
+        } else {
+            mousePos.x = e.clientX;
+            mousePos.y = e.clientY;
+        }
     });
 
-    window.addEventListener('mousedown', function() {
+    canvas.addEventListener(startEvent, function() {
         isMouseDown = true;
         circleRadius = 1;
     });
 
-    window.addEventListener('mouseup', function() {
+    canvas.addEventListener(endEvent, function() {
         isMouseDown = false;
     });
 
